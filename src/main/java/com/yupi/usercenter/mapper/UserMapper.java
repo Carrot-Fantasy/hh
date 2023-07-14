8e47508cd41434c69bbada5f508e8dd58acc43f2
isCyril <1374312358@qq.com>
2023-07-14 20:06:47 +0800

    update 支持生产环境

---
+++ src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.usercenter.mapper;
+
+import com.yupi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 13743
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2023-07-11 13:43:09
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
